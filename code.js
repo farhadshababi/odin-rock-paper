d885e5683d96d995a20efb47f60bd4ccde83c39c
Farhad <farhad.shababi@gmail.com>
2022-11-18 23:06:54 -0800

    rock-paper

---
+++ code.js
@@ -0,0 +1,27 @@
+
+function getComputerChoice (){
+    return Math.floor(Math.random() * 3);
+};
+
+let computerChoice = getComputerChoice ();
+
+if (computerChoice === 0) {
+    computerChoice="Rock"}
+else if (computerChoice === 1){ 
+    computerChoice="Paper"}
+else {
+    computerChoice="Scissor"}; 
+
+console.log (computerChoice);
+
+let playerChoice;
+
+function playROund (playerChoice, computerChoice){
+  
+    
+    
+    
+    
+
+}
+
