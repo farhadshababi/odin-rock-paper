7a820229b45abe8eeff6060c1e1c0e02ae409069
Farhad <farhad.shababi@gmail.com>
2022-11-19 19:22:47 -0800

    Pushed RPS

--- code.js
+++ code.js
@@ -1,27 +1,61 @@
-
-function getComputerChoice (){
-    return Math.floor(Math.random() * 3);
-};
-
-let computerChoice = getComputerChoice ();
-
-if (computerChoice === 0) {
-    computerChoice="Rock"}
-else if (computerChoice === 1){ 
-    computerChoice="Paper"}
-else {
-    computerChoice="Scissor"}; 
-
-console.log (computerChoice);
-
-let playerChoice;
-
-function playROund (playerChoice, computerChoice){
-  
-    
-    
-    
-    
-
+function getComputerChoice() {
+  let computerChoiceInt = Math.floor(Math.random() * 3);
+  if (computerChoiceInt === 0) {
+    return "ROCK";
+  } else if (computerChoiceInt === 1) {
+    return "PAPER";
+  } else {
+    return "SCISSOR";
+  }
 }
 
+let playerChoice;
+let gameResult = "No game";
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerChoice, computerChoice) {
+  if (
+    (playerChoice.toUpperCase() === "SCISSOR" &&
+      computerChoice.toUpperCase() === "PAPER") ||
+    (playerChoice.toUpperCase() === "PAPER" &&
+      computerChoice.toUpperCase() === "ROCK") ||
+    (playerChoice.toUpperCase() === "ROCK" &&
+      computerChoice.toUpperCase() === "SCISSOR")
+  ) {
+    gameResult = `You Win! ${playerChoice} beats ${computerChoice}. `;
+    playerScore++;
+  } else if (
+    (computerChoice.toUpperCase() === "SCISSOR" &&
+      playerChoice.toUpperCase() === "PAPER") ||
+    (computerChoice.toUpperCase() === "PAPER" &&
+      playerChoice.toUpperCase() === "ROCK") ||
+    (computerChoice.toUpperCase() === "ROCK" &&
+      playerChoice.toUpperCase() === "SCISSOR")
+  ) {
+    gameResult = `You lost! ${computerChoice} beats ${playerChoice}. `;
+    computerScore++;
+  } else {
+    gameResult = "It is a tie. ";
+  }
+}
+
+function game(numRounds) {
+  for (let i = 0; i < numRounds; i++) {
+    playerChoice = prompt("What is your weapon of choice");
+    let computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+    console.log(playerScore, computerScore);
+  }
+
+  if (computerScore > playerScore) {
+    finalResult = "You Suck";
+  } else if (playerScore > computerScore) {
+    finalResult = "YYou Rock";
+  } else {
+    finalResult = "Finished game but tie";
+  }
+  console.log(finalResult);
+}
+
+game(5);
